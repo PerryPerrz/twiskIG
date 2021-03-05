923b1f0ef67bfef4b3edf4b7fb0e547c02d2ce61
iopeti2u <hugo.iopeti5@etu.univ-lorraine.fr>
2021-03-05 09:58:47 +0100

    FabriqueIdentifiantTest

---
+++ src/twisk/tests/FabriqueIdentifiantTest.java
@@ -0,0 +1,33 @@
+package twisk.tests;
+
+import twisk.outils.FabriqueIdentifiant;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FabriqueIdentifiantTest {
+    FabriqueIdentifiant fabID;
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        fabID = FabriqueIdentifiant.getInstance();
+    }
+
+    @org.junit.jupiter.api.Test
+    void getIdentifiantEtape() {
+        String id = fabID.getIdentifiantEtape();
+        assertEquals(id,"0");
+
+        id = fabID.getIdentifiantEtape();
+        assertEquals(id,"1");
+
+        id = fabID.getIdentifiantEtape();
+        assertEquals(id,"2");
+
+        id = fabID.getIdentifiantEtape();
+        assertEquals(id,"3");
+
+        id = fabID.getIdentifiantEtape();
+        id = fabID.getIdentifiantEtape();
+        id = fabID.getIdentifiantEtape();
+        assertEquals(id,"6");
+    }
+}
