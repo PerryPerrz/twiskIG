411c0d6e172ceb4853f301946a715a9f4e53e502
iopeti2u <hugo.iopeti5@etu.univ-lorraine.fr>
2021-03-18 12:01:23 +0100

    tests

---
+++ src/twisk/tests/EtapeIGTest.java
@@ -0,0 +1,18 @@
+package twisk.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import twisk.mondeIG.EtapeIG;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EtapeIGTest {
+    EtapeIG etape;
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void randomPositions() {
+    }
+}
